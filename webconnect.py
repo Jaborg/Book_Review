9751479c00536c624ec93f72bfc66124dea2f465
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2021-12-26 17:54:58 +0000

    first commit

---
+++ webconnect.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+
+#orm
+
+
+
+@app.get("/")
+async def index(request : Request):
+
+
+    return templates.TemplateResponse("frontpage.html",
+        {"request":request })
