86b59aa697b0d23e6ee29a475f343e7e4b6a2044
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2022-01-12 17:38:56 +0000

    Adding in href for changing page view

--- webconnect.py
+++ webconnect.py
@@ -10,5 +10,13 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def index(request: Request):
-
+    print('Ypoooo')
     return templates.TemplateResponse("frontpage.html", {"request": request})
+
+
+
+@app.get("/information")
+async def info(request: Request):
+    print('hello')
+
+    return templates.TemplateResponse("information.html", {"request": request})
