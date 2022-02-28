7bc12c85d405053df828430a9dd27f3e9ce63590
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2022-02-28 15:55:09 +0000

    Added in external css functionality

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 application = FastAPI()
+application.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
