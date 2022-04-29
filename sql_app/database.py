6b44eda2002ce4b22ecb9a891ec3af45092fde75
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2022-04-29 20:02:26 +0100

    Adding in connection to aws psg

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,11 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+import os
+pg_username = os.getenv("PG_USERNAME", "DEFAULT")
+pg_password = os.getenv("PG_PASSWORD", "Secret")
+host = os.getenv("PG_HOST","DEFAULT")
+db_name = os.getenv("PG_DB","DEFAULT")
+db_string = f"postgresql://{pg_username}:{pg_password}@{host}:5432/{db_name}"
+
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
