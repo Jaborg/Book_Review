6ae876ccaa3a0a7974e3501445e67e1dda6d74e5
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2023-05-01 22:24:48 +0100

    Added in fake test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-import application
+from application import application
 
 client = TestClient(application)
 
@@ -8,4 +8,4 @@ def test_index():
     response = client.get("/")
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
-    assert "Welcome to the Frontpage!" in response.text
+    assert "Introspection is a tool to carve out serenity" in response.text
