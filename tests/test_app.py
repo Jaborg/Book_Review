4e79e951a73775546852bdac5988a9bcbce23bb9
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2023-05-01 22:01:55 +0100

    Added in fake test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,11 @@
-def test_app():
-    """TODO: Put in a test and get it passing."""
-    assert False
+from fastapi.testclient import TestClient
+import application
+
+client = TestClient(application)
+
+
+def test_index():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
+    assert "Welcome to the Frontpage!" in response.text
